8026ec1942ccf84da72c37d266ccf5d425a7cc7e
AngelManuelSaenzHernandez <asaenzhdez@gmail.com>
2026-07-05 19:04:10 -0600

    Añade dependencias y clase de conexión MySQL
    Se agregan referencias a nuevas librerías externas mediante NuGet, se crea el archivo packages.config, se añade la clase Conexion.cs para gestionar la conexión a MySQL y se actualiza App.config con binding redirects y ajustes en la cadena de conexión.

---
+++ Conexion.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Punto.Forms
+{
+    internal class Conexion
+    {
+        private readonly string Cadena;
+
+        public Conexion()
+        {
+            Cadena = "Server=127.0.0.1;Database=PuntoDB;Uid=root;Pwd=;Port=3306;";
+        }
+
+        public MySqlConnection GetConeccion()
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection(Cadena);
+                con.Open();
+                return con;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error al conectar con MySQL:\n" + ex.Message);
+                return null;
+            }
+        }
+    }
+}
